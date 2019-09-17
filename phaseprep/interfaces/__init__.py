f0c177353e3189eb5290672d3dbc2373205d072a
Olivia Stanley <oliviawstanley@gmail.com>
2019-09-17 10:40:18 -0400

    added memory efficient version of PhaseFitOdr

--- phaseprep/interfaces/__init__.py
+++ phaseprep/interfaces/__init__.py
@@ -2,3 +2,4 @@ from .DetrendMag import DetrendMag
 from .PreprocessPhase import PreprocessPhase
 from .RestAverage import RestAverage
 from .PhaseFitOdr import PhaseFitOdr
+from .PhaseFitOdrChunked import PhaseFitOdrChunked
